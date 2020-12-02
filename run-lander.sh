fc2fa63fe24ad442f9a999894713ed3cf9bc000b
Ingo Wald <ingowald@gmail.com>
2020-12-01 17:53:10 -0700

    updated lander export script

--- run-lander.sh
+++ run-lander.sh
@@ -2,11 +2,19 @@
     -o  /mnt/raid/wald/models/umesh/lander-small-vmag-9000.umesh\
     /mnt/raid/wald/models/unstructured/lander-small/geometry/dAgpu0145_Fa_me\
     --scalars /mnt/raid/wald/models/unstructured/lander-small/10000unsteadyiters/dAgpu0145_Fa_volume_data.\
+    -var vort_mag -ts 9000
+
+./umeshImportLanderFun3D\
+    -o  /mnt/raid/wald/models/umesh/lander-small-vmag-9000-with-surf.umesh\
+    /mnt/raid/wald/models/unstructured/lander-small/geometry/dAgpu0145_Fa_me\
+    --scalars /mnt/raid/wald/models/unstructured/lander-small/10000unsteadyiters/dAgpu0145_Fa_volume_data.\
     -var vort_mag -ts 9000\
     -surf /mnt/raid/wald/models/unstructured/a-143m/dAgpu0145_Fa.lb8.ugrid64
-cp /mnt/raid/wald/models/umesh/lander-small-vmag-9000.umesh /mnt/raid/wald/nfs/upload/
 
-./umeshDumpSurfaceMesh /mnt/raid/wald/models/umesh/lander-small-vmag-9000.umesh -o /mnt/raid/wald/models/umesh/lander-small-surface.obj
+cp /mnt/raid/wald/models/umesh/lander-small-vmag-9000.umesh /mnt/raid/wald/nfs/upload/
+cp /mnt/raid/wald/models/umesh/lander-small-vmag-9000-with-surf.umesh /mnt/raid/wald/nfs/upload/
+
+./umeshDumpSurfaceMesh /mnt/raid/wald/models/umesh/lander-small-vmag-9000-with-surf.umesh -o /mnt/raid/wald/models/umesh/lander-small-surface.obj
 cp /mnt/raid/wald/models/umesh/lander-small-surface.obj /mnt/raid/wald/nfs/upload/
 
 
@@ -18,10 +26,18 @@ cp /mnt/raid/wald/models/umesh/lander-small-surface.obj /mnt/raid/wald/nfs/uploa
     --scalars /mnt/raid/wald/models/unstructured/lander-huge/1000unsteadyiters/114B_volume_data.\
     -var vort_mag -ts 900
 
+./umeshImportLanderFun3D\
+    -o  /mnt/raid/wald/models/umesh/lander-huge-vmag-900-with-surf.umesh\
+    /mnt/raid/wald/models/unstructured/lander-huge/geometry/114B_me\
+    --scalars /mnt/raid/wald/models/unstructured/lander-huge/1000unsteadyiters/114B_volume_data.\
+    -surf /mnt/raid/wald/models/unstructured/lander-577/577M.lb8.ugrid64\
+    -var vort_mag -ts 900
+
 #    -surf /mnt/raid/wald/models/unstructured/a-143m/dAgpu0145_Fa.lb8.ugrid64
 
 cp /mnt/raid/wald/models/umesh/lander-huge-vmag-900.umesh /mnt/raid/wald/nfs/upload/
-./umeshDumpSurfaceMesh /mnt/raid/wald/models/umesh/lander-huge-vmag-9000.umesh\
+cp /mnt/raid/wald/models/umesh/lander-huge-vmag-900-with-surf.umesh /mnt/raid/wald/nfs/upload/
+./umeshDumpSurfaceMesh /mnt/raid/wald/models/umesh/lander-huge-vmag-9000-with-surf.umesh\
 		       -o /mnt/raid/wald/models/umesh/lander-huge-surface.obj
 cp /mnt/raid/wald/models/umesh/lander-huge-surface.obj /mnt/raid/wald/nfs/upload/
 
